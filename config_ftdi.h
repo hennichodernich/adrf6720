b8bae4b91ef001d11315f696bd53be8a217b56cd
Henning Paul <paul@oslo.ant.uni-bremen.de>
2017-01-11 15:11:40 +0100

    Changed pin assignment.

--- config_ftdi.h
+++ config_ftdi.h
@@ -1,9 +1,9 @@
 #ifndef CONFIG_FTDI_H
 #define CONFIG_FTDI_H
 
-#define CS 0
-#define SCLK 2
-#define SDIO 4
+#define CS 0		//TXD, DBUS0
+#define SCLK 2		//RTS, DBUS2
+#define SDIO 4		//DTR, DBUS4
 
 #define FTDI_INTERFACE INTERFACE_A
 
