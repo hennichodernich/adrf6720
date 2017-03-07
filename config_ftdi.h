6f2d7565ea9bab67f093ced985f642aedc2be806
Henning Paul <paul@oslo.ant.uni-bremen.de>
2017-03-07 15:25:37 +0100

    Added alternative pinout.

--- config_ftdi.h
+++ config_ftdi.h
@@ -1,9 +1,21 @@
 #ifndef CONFIG_FTDI_H
 #define CONFIG_FTDI_H
 
+#undef DEADBUG
+
+#ifdef DEADBUG
+
 #define CS 0		//TXD, DBUS0
 #define SCLK 2		//RTS, DBUS2
 #define SDIO 4		//DTR, DBUS4
+
+#else
+
+#define CS 4		//TXD, DBUS0
+#define SCLK 2		//RTS, DBUS2
+#define SDIO 0		//DTR, DBUS4
+
+#endif
 
 #define FTDI_INTERFACE INTERFACE_A
 
