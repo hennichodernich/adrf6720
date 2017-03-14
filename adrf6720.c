5509f056ec3c076a7f3843e3a9f85e224c1b3db3
Henning Paul <paul@oslo.ant.uni-bremen.de>
2017-03-14 14:03:25 +0100

    Testing things.

--- adrf6720.c
+++ adrf6720.c
@@ -9,23 +9,13 @@
 #include "adrf6720.h"
 
 #define NUM_REGS 21
-#define WRITE_LENGTH 16
+#define WRITE_LENGTH 17
 
 typedef struct {
     int reset;
     int dump;
     int poweroff;
     int nothing;
-    int divider_int;
-    int divider_frac;
-    int divider_mod;
-    int refsel;
-    int fractional_mode;
-    int cp_cscale;
-    int cp_bleed;
-    int abldly;
-    int cp_ctrl;
-    int pfd_clk_edge;
 } t_opt_struct;
 
 
@@ -42,11 +32,12 @@ static void print_usage(const char *prog)
          "  -f --frac     fractional part of divider, numerator (int). Default 0.\n"
          "  -m --mod      fractional part of divider, denominator (int). Default n/a (integer mode).\n"
          "  -s --refsel   clock reference selector (int). Default 1 (x1).\n"
-         "  -c --cscale   charge pump coarse scale current. Default 4 (1000uA).\n"
-         "  -b --bleed    charge pump bleed current. Default 32 (0uA).\n"
+         "  -c --cscale   charge pump coarse scale current. Default ? (?uA).\n"
+         "  -b --bleed    charge pump bleed current. Default ?? (?uA).\n"
          "  -a --abldly   anti-backlash delay. Default 0.\n"
-         "  -l --cpctrl   charge pump control. Default 0 (both on).\n"
-         "  -e --clkedge  PFD clock edge. Default 0 (both down).\n"
+         "  -l --cpctrl   charge pump control. Default ? (both on).\n"
+         "  -e --clkedge  PFD clock edge. Default ? (both down).\n"
+         "  -k --refclk   reference clock in MHz. Default 32MHz.\n"
          "  -r --reset    reset chip first\n"
          "  -d --dump     dump registers\n"
          "  -n --nothing  do nothing (useful for plain reset or dump)\n"
@@ -66,6 +57,7 @@ int parse_opts(int argc, char *argv[], t_opt_struct *opt_struct, t_adrf6720_sett
         { "abldly",   1, 0, 'a' },
         { "cpctrl",   1, 0, 'l' },
         { "clkedge",  1, 0, 'e' },
+        { "refclk",   1, 0, 'k' },
         { "reset",    0, 0, 'r' },
         { "dump",     0, 0, 'd' },
         { "nothing",  0, 0, 'n' },
@@ -77,7 +69,7 @@ int parse_opts(int argc, char *argv[], t_opt_struct *opt_struct, t_adrf6720_sett
 
     while (1) {
 
-        c = getopt_long(argc, argv, "i:f:m:s:c:b:rdno", lopts, NULL);
+        c = getopt_long(argc, argv, "i:f:m:s:c:b:k:rdno", lopts, NULL);
 
         if (c == -1)
             break;
@@ -111,6 +103,9 @@ int parse_opts(int argc, char *argv[], t_opt_struct *opt_struct, t_adrf6720_sett
             break;
         case 'e':
             (*settings).CLKEDGE=atoi(optarg);
+            break;
+        case 'k':
+            (*settings).pll_ref_in=atof(optarg);
             break;
         case 'r':
             (*opt_struct).reset=1;
@@ -168,17 +163,18 @@ int main(int argc, char* argv[])
 
     uint8_t writeorder[WRITE_LENGTH] = {
         ADRF6720_ENABLES,
-	ADRF6720_BALUN_CTL,
-	ADRF6720_MOD_LIN_CTL,
-	ADRF6720_MOD_CTL0,
-	ADRF6720_MOD_CTL1,
-	ADRF6720_DITH_CTL1,
-	ADRF6720_DITH_CTL2,
         ADRF6720_CP_CTL,
-        ADRF6720_PFD_CTL,
-        ADRF6720_PFD_CP_CTL,
-        ADRF6720_VCO_CTL2,
         ADRF6720_VCO_CTL3,
+	      ADRF6720_BALUN_CTL,
+	      ADRF6720_MOD_LIN_CTL,
+	      ADRF6720_MOD_CTL0,
+	      ADRF6720_MOD_CTL1,
+        ADRF6720_PFD_CP_CTL,
+	      ADRF6720_DITH_CTL1,
+	      ADRF6720_DITH_CTL2,
+        ADRF6720_CALIBRATION
+        ADRF6720_VCO_CTL2,
+        ADRF6720_PFD_CTL,
         ADRF6720_VCO_CTL,
         ADRF6720_INT_DIV,
         ADRF6720_FRAC_DIV,
@@ -189,54 +185,65 @@ int main(int argc, char* argv[])
     program_options.reset=0;
     program_options.poweroff=0;
     program_options.nothing=0;
-    //(*opt_struct).divider_int=75;
-    //(*opt_struct).divider_frac=0;
-    //(*opt_struct).divider_mod=0;
 
-
-
+    //reg 0x02
     settings.DIV_MODE=1;
     settings.INT=76;
+    //reg 0x03
     settings.FRAC=384;
+    //reg 0x04
     settings.MOD=1536;
+    //reg 0x20
     settings.cscale_val=1;
-    settings.BLEED=5;
+    settings.BLEED=38;
     settings.FSCALE=0;
+    //reg 0x21
     settings.REF_MUX_SEL=0;
     settings.PFD_Polarity=1;
     settings.REF_SEL=1;
+    //reg 0x22
     settings.VCO_SEL=0;
     settings.LO_DRV_LVL=0;  //1x internal LO mode
     settings.DRVDIV2_EN=0;  //1x internal LO mode
     settings.DIV8_EN=0;
     settings.DIV4_EN=0;
     settings.VCO_LDO_R2SEL=10;
-    settings.VCO_LDO_R4SEL=3;
+    settings.VCO_LDO_R4SEL=2;
+    //reg 0x30
     settings.BAL_COUT=0;
     settings.BAL_CIN=0;
-    settings.MOD_CDAC=0;
-    settings.MOD_RDAC=32;
+    //reg 0x31
+    settings.MOD_CDAC=1;
+    settings.MOD_RDAC=68;
+    //reg 0x32
     settings.POLi=2;
     settings.POLq=1;
     settings.QLO=0;
     settings.ILO=0;
+    //reg 0x33
     settings.DCOFFI=0;
     settings.DCOFFQ=0;
-    settings.CLKEDGE=0;
+    //reg 0x40
+    settings.CLKEDGE=2;
     settings.CPCTRL=0;
     settings.ABLDLY=0;
+    //reg 0x42,0x43
     settings.DITH_EN=1;
     settings.DITH_MAG=3;
     settings.DITH_VAL=0;
-    settings.VTUNE_CTRL=1;
-    settings.VCO_BAND_SRC=1;
-    settings.BAND=32;
+    //reg 0x44
     settings.SDM_DIVD_CLR=0;
     settings.BANDCAL=0;
-    settings.VTUNE_DAC_OFFSET=180;
-    settings.VTUNE_DAC_SLOPE=10;
+    //reg 0x45
+    settings.VTUNE_CTRL=0;
+    settings.VCO_BAND_SRC=0;
+    settings.BAND=0;
+    //reg 0x49
+    settings.VTUNE_DAC_OFFSET=0;
+    settings.VTUNE_DAC_SLOPE=0;
+
     settings.pll_ref_in=32.0;
-    settings.pll_ref_div=4.0;
+    settings.pll_ref_div=1.0;
     settings.lo_out_freq=2440.0;
 
     retval = parse_opts(argc, argv, &program_options, &settings);
@@ -416,6 +423,7 @@ int main(int argc, char* argv[])
                                   | ADRF6720_BITS_MOD_CSEL(settings.MOD_CDAC);
 
         regs[ADRF6720_CP_CTL] = ADRF6720_BITS_CP_CSCALE(((1 << settings.cscale_val) - 1))
+                              | ADRF6720_BITS_CP_FSCALE(settings.FSCALE)
                               | ADRF6720_BITS_CP_BLEED(settings.BLEED);
 
 
